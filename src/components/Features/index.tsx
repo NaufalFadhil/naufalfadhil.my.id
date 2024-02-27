842c480723602c01feb0627f9734a094a5513fe6
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-02-27 18:37:08 +0700

    fix(home): fixed navigation link for all features

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Style from '../../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Features() {
   return (
@@ -10,34 +11,46 @@ export default function Features() {
             </h2>
             <div className="row row-cols-2 row-cols-md-3 row-cols-lg-6">
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-achievement.svg" alt="button achievement" width={180} height={180} />
-                    </div>
+                    <Link href="/achievements">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable2x" src="assets/images/button/button-achievement.svg" alt="button achievement" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-sosmed.svg" alt="button sosmed" width={180} height={180} />
-                    </div>
+                    <Link href="/link">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable1x" src="assets/images/button/button-sosmed.svg" alt="button sosmed" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-project.svg" alt="button project" width={180} height={180} />
-                    </div>
+                    <Link href="/projects">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable1x" src="assets/images/button/button-project.svg" alt="button project" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-skill.svg" alt="button skill" width={180} height={180} />
-                    </div>
+                    <Link href="/skills">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable1x" src="assets/images/button/button-skill.svg" alt="button skill" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-asset.svg" alt="button asset" width={180} height={180} />
-                    </div>
+                    <Link href="/assets">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable1x" src="assets/images/button/button-asset.svg" alt="button asset" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
                 <div className="col">
-                    <div className={Style.imageContainer}>
-                        <Image className="images" src="assets/images/button/button-support.svg" alt="button project" width={180} height={180} />
-                    </div>
+                    <Link href="/supports">
+                        <div className={Style.imageContainer}>
+                            <Image className="images p-3 clickable1x" src="assets/images/button/button-support.svg" alt="button project" width={180} height={180} />
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
