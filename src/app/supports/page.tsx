8efa05be76774a5f3d5d46f2eeec3078bb54445b
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-02-28 00:08:31 +0700

    feat(support): create supports page

---
+++ src/app/supports/page.tsx
@@ -0,0 +1,35 @@
+import Footer from '@/components/Footer'
+import Navigation from '@/components/Navigation'
+import React from 'react'
+
+export default function Supports() {
+  return (
+    <>
+      <main>
+          <div className="container">
+              <Navigation />
+              <section id="support-page" className="container" style={{ marginTop: '8rem' }}>
+                  <h1 className="text-center">Support Me</h1>
+                  <div className="row">
+                      <div className="col-sm-6">
+                          <a href="https://bit.ly/NaufalFadhil-Saweria">
+                              <div className="card m-5">
+                                  <h1 className="text-center">saweria.co</h1>
+                              </div>
+                          </a>
+                      </div>
+                      <div className="col-sm-6">
+                          <a href="https://bit.ly/NaufalFadhil-PayPal">
+                              <div className="card m-5">
+                                  <h1 className="text-center">Paypal</h1>
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+              </section>
+              <Footer />
+          </div>
+      </main>
+    </>
+  )
+}
