1b2aa95b2372ae4139efb39cd1ea0ba742969c59
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-02-28 13:22:44 +0700

    feat(skills): create skill page

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,321 @@
+import Footer from '@/components/Footer'
+import Navigation from '@/components/Navigation'
+import React from 'react'
+
+export default function Skills() {
+    return (
+        <div className='container'>
+            <Navigation />
+            <main className="main-skills mb-5 main-section">
+                <h1 className="text-center">My Skill</h1>
+                <div className="card-group">
+                    <div className="card top-skill-card m-3 text-center">
+                        <a href="#website">
+                            <img src="../assets/images/logo/globe.svg" className="card-img-top img-center" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Website</h5>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">PROFICIENT</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="card top-skill-card m-3 text-center">
+                        <a href="#android">
+                            <img src="../assets/images/logo/android.svg" className="card-img-top img-center" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Android</h5>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">ADVANCE BEGINNER</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="card top-skill-card m-3 text-center">
+                        <a href="#design">
+                            <img src="../assets/images/logo/color-palette.svg" className="card-img-top img-center" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Design</h5>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">ADVANCE BEGINNER</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="card top-skill-card m-3 text-center">
+                        <a href="#desktop">
+                            <img src="../assets/images/logo/desktop.svg" className="card-img-top img-center" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Desktop</h5>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">NOVICE</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="card top-skill-card m-3 text-center">
+                        <a href="#cyber-security">
+                            <img src="../assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Cyber Security</h5>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">NOVICE</small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="website">Website</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/html.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">HTML</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/css-3.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">CSS</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/js.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">JavaScript</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/php.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">PHP</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/bootstrap.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Bootstrap</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/sass.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">SASS/SCSS</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/laravel.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Laravel</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/codeigniter.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <p></p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/react.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title mt-3">React JS</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="android">Android</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Java</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/kotlin.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Kotlin</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="design">Design</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/figma-icon.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title mt-3">Figma</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/adobe_illustrator-icon.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title mt-3">Illustrator</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/blender.svg" height="130" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Blender</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="desktop">Desktop</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Java</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/visual-studio.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">VB</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">PROJECT BASED</small>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="android">Database Management System</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/mySQL.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">MySQL</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/postgresql-icon.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">PostgreSQL</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/oracle.svg" className="mt-5 card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title" style={{ marginTop: '35px' }}>Oracle</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                </div>
+                <h2 className="anchor-adjust" id="cyber-security">Cyber Security</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Web Security</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Forensics</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Cryptography</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                </div>
+                
+                <h2 className="anchor-adjust" id="more-language">More Language</h2>
+                <div className="card-group">
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/python.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Python</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">CERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/cpp.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">C++</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                    <div className="card m-3 text-center">
+                        <img src="../assets/images/logo/c.svg" className="card-img-top img-center" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">C</h5>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">UNCERTIFIED</small>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <Footer />
+        </div>
+    )
+}
