2f0708a711143101b2eb3b1186e8a7f670d2429d
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-02-28 13:26:22 +0700

    fix(skills): fixed relative path of image at skill

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -11,7 +11,7 @@ export default function Skills() {
                 <div className="card-group">
                     <div className="card top-skill-card m-3 text-center">
                         <a href="#website">
-                            <img src="../assets/images/logo/globe.svg" className="card-img-top img-center" alt="..." />
+                            <img src="/assets/images/logo/globe.svg" className="card-img-top img-center" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Website</h5>
                             </div>
@@ -22,7 +22,7 @@ export default function Skills() {
                     </div>
                     <div className="card top-skill-card m-3 text-center">
                         <a href="#android">
-                            <img src="../assets/images/logo/android.svg" className="card-img-top img-center" alt="..." />
+                            <img src="/assets/images/logo/android.svg" className="card-img-top img-center" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Android</h5>
                             </div>
@@ -33,7 +33,7 @@ export default function Skills() {
                     </div>
                     <div className="card top-skill-card m-3 text-center">
                         <a href="#design">
-                            <img src="../assets/images/logo/color-palette.svg" className="card-img-top img-center" alt="..." />
+                            <img src="/assets/images/logo/color-palette.svg" className="card-img-top img-center" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Design</h5>
                             </div>
@@ -44,7 +44,7 @@ export default function Skills() {
                     </div>
                     <div className="card top-skill-card m-3 text-center">
                         <a href="#desktop">
-                            <img src="../assets/images/logo/desktop.svg" className="card-img-top img-center" alt="..." />
+                            <img src="/assets/images/logo/desktop.svg" className="card-img-top img-center" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Desktop</h5>
                             </div>
@@ -55,7 +55,7 @@ export default function Skills() {
                     </div>
                     <div className="card top-skill-card m-3 text-center">
                         <a href="#cyber-security">
-                            <img src="../assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
+                            <img src="/assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Cyber Security</h5>
                             </div>
@@ -68,7 +68,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="website">Website</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/html.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/html.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">HTML</h5>
                         </div>
@@ -77,7 +77,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/css-3.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/css-3.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">CSS</h5>
                         </div>
@@ -86,7 +86,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/js.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/js.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">JavaScript</h5>
                         </div>
@@ -95,7 +95,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/php.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/php.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">PHP</h5>
                         </div>
@@ -104,7 +104,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/bootstrap.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/bootstrap.svg" height="110" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Bootstrap</h5>
                         </div>
@@ -113,7 +113,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/sass.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/sass.svg" height="110" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">SASS/SCSS</h5>
                         </div>
@@ -124,7 +124,7 @@ export default function Skills() {
                 </div>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/laravel.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/laravel.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Laravel</h5>
                         </div>
@@ -133,7 +133,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/codeigniter.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/codeigniter.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <p></p>
                         </div>
@@ -142,7 +142,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/react.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/react.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title mt-3">React JS</h5>
                         </div>
@@ -154,7 +154,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="android">Android</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Java</h5>
                         </div>
@@ -163,7 +163,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/kotlin.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/kotlin.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Kotlin</h5>
                         </div>
@@ -175,7 +175,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="design">Design</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/figma-icon.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/figma-icon.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title mt-3">Figma</h5>
                         </div>
@@ -184,7 +184,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/adobe_illustrator-icon.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/adobe_illustrator-icon.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title mt-3">Illustrator</h5>
                         </div>
@@ -193,7 +193,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/blender.svg" height="130" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/blender.svg" height="130" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Blender</h5>
                         </div>
@@ -205,7 +205,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="desktop">Desktop</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/java.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Java</h5>
                         </div>
@@ -214,7 +214,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/visual-studio.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/visual-studio.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">VB</h5>
                         </div>
@@ -226,7 +226,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="android">Database Management System</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/mySQL.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/mySQL.svg" height="110" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">MySQL</h5>
                         </div>
@@ -235,7 +235,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/postgresql-icon.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/postgresql-icon.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">PostgreSQL</h5>
                         </div>
@@ -244,7 +244,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/oracle.svg" className="mt-5 card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/oracle.svg" className="mt-5 card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title" style={{ marginTop: '35px' }}>Oracle</h5>
                         </div>
@@ -256,7 +256,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="cyber-security">Cyber Security</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/hacker.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Web Security</h5>
                         </div>
@@ -265,7 +265,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Forensics</h5>
                         </div>
@@ -274,7 +274,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/hacker.svg" height="110" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Cryptography</h5>
                         </div>
@@ -287,7 +287,7 @@ export default function Skills() {
                 <h2 className="anchor-adjust" id="more-language">More Language</h2>
                 <div className="card-group">
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/python.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/python.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Python</h5>
                         </div>
@@ -296,7 +296,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/cpp.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/cpp.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">C++</h5>
                         </div>
@@ -305,7 +305,7 @@ export default function Skills() {
                         </div>
                     </div>
                     <div className="card m-3 text-center">
-                        <img src="../assets/images/logo/c.svg" className="card-img-top img-center" alt="..." />
+                        <img src="/assets/images/logo/c.svg" className="card-img-top img-center" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">C</h5>
                         </div>
