80b1894923a9c1216100790ee02d9c7811150446
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-06-19 14:42:04 +0700

    feat(resume): attach google drive link

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,7 +4,8 @@ import React, { useEffect } from 'react'
 
 export default function Resume() {
     useEffect(() => {
-        window.location.href = './assets/files/Resume - Naufal Fadhil Athallah.pdf'
+        // window.location.href = './assets/files/Resume - Naufal Fadhil Athallah.pdf'
+        window.location.href = 'https://drive.google.com/file/d/1ZH8_J-DpiKzwKYjEShlcl1lC-aJtaK31/view?usp=sharing'
     }, [])
 
     return <div> Loading Resume... </div>
